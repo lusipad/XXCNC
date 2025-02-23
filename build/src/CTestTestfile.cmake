6e505945023304aad1091a394ba9fcd91f794073
lusipad <lusipad@163.com>
2025-02-24 00:58:56 +0800

    chore: delete build files

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repos/XXCNC/src
-# Build directory: D:/Repos/XXCNC/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Repos/XXCNC/tests
-# Build directory: D:/Repos/XXCNC/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/Repos/XXCNC/build/tests/xxcnc_tests[1]_include.cmake")
